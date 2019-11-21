4533e0e7960830b9df3273c2c98b813603d8a684
Garrett Ewens <garrettewens@live.com>
2019-11-20 19:38:39 -0600

    Support colon-separated output format - JPT-9

---
+++ src/main/java/com/xellitix/jenkins/plugintool/output/JenkinsPluginListOutputWriter.java
@@ -0,0 +1,37 @@
+package com.xellitix.jenkins.plugintool.output;
+
+import com.xellitix.jenkins.plugintool.plugin.Plugin;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+/**
+ * JENKINS {@link PluginListOutputWriter} implementation.
+ *
+ * @author Garrett Ewens
+ */
+public class JenkinsPluginListOutputWriter implements PluginListOutputWriter {
+  /**
+   * Gets the {@link PluginListOutputFormat}.
+   *
+   * @return The {@link PluginListOutputFormat}.
+   */
+  @Override
+  public PluginListOutputFormat getFormat() {
+    return PluginListOutputFormat.JENKINS;
+  }
+
+  /**
+   * Writes a {@link Plugin} {@link List}.
+   *
+   * @param plugins The {@link Plugin}s.
+   * @param output  The {@link OutputStream}.
+   */
+  @Override
+  public void write(final List<Plugin> plugins, final PrintStream output) {
+    plugins.forEach((value) -> {
+      System.out.println(value.getName() + ":" + value.getVersion());
+    });
+  }
+}
---
+++ src/test/java/com/xellitix/jenkins/plugintool/output/JenkinsPluginListOutWriterTest.java
@@ -0,0 +1,4 @@
+package com.xellitix.jenkins.plugintool.output;
+
+public class JenkinsPluginListOutWriterTest {
+}
